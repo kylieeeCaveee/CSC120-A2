0bbbdff6b5b5411df50dedfde0a38244b75fccc7
kylie <kcave@smith.edu>
2025-02-05 22:49:41 -0500

    day 2 work

--- computer.py
+++ computer.py
@@ -1,7 +1,7 @@
 class Computer:
     #This is the computer class- it has the attributes: desciption, processor_type, hard_drive_capactiy, 
     # memory, operating_system, year_made, price. It contains the methods update_price, update_OS, 
-    # and refurbish.
+    # and refurbish, and print_details. 
     description:str
     processor_type:str
     hard_drive_capacity: int
@@ -9,6 +9,7 @@ class Computer:
     operating_system: str
     year_made:int
     price: int
+    
    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
@@ -21,6 +22,7 @@ class Computer:
         self.operating_system= os
         self.year_made= ym
         self.price= p
+        
     
     def update_price(new_price: int): 
         #this updates the price of the computer to the price given to the function
@@ -30,19 +32,34 @@ class Computer:
         #this updates the OS of the computer to the new OS given
         self.operating_system= new_OS
 
-    def refurbish(new_OS, new_price): 
+    def refurbish(new_os: Optional[str] = None): 
         #this function refurbishes the computer which updates the OS and the price
-        update_price(new_price)
-        update_OS (new_OS)
+        if self.year_made < 2000:
+             self.price = 0 # too old to sell, donation only
+        elif self.year_made < 2012:
+             self.price= 250 # heavily-discounted price on machines 10+ years old
+        elif self.year_made < 2018:
+             self.price = 550 # discounted price on machines 4-to-10 year old machines
+        else:
+             self.price= 1000 # recent stuff
+
+        if new_os is not None:
+             self.operating_system = new_os # update details after installing new OS
+    
+    def print_details(): 
+        #this function prints all the attributes of a computer
+        print ("Description:", self.description, "Proccessor:", self.processor, "Hard Drive Capacity:", self.hard_drive_capacity, "Memory", self.memory, "Operating System:", self.operating_system, "Year:", self.year_made, "Price:", self.price)
 
 def main(): 
     myComputer: Computer= Computer("my mac","some processor", 1024, 64 ,"old OS", 2013, 1500)
     print (myComputer)
     myComputer.update_price("1000000")
     myComputer.update_OS("newest OS")
+    print(myComputer)
+    myComputer.refurbish("epic OS")
     print (myComputer)
 
 if __name__== "__main__": 
     main()
 
-    # What methods will you need?
+   
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,32 @@
+import computer
+
 class ResaleShop:
+    #this is the class ResaleShop, it has the attribute inventory and the buy and sell and print inventory methods
+    inventory: list
 
-    # What attributes will it need?
+    def __init__( ivn: list): 
+        #this initalizes any new resale shops
+        self.inventory= ivn
+    
+    def buy(comp: computer): 
+        #this buys the computer and adds it to the stores inventory
+        self.inventory.append(comp)
+        return self.inventory.index(comp)
+    
+    def sell(comp: computer):
+        #this sells the computer and removes it from the stores inventory
+        if comp in self.inventory:  
+            self.inventory.pop(comp)
+            print("Item", comp, "sold!")
+        else: 
+            print("Item", comp, "not found. Please select another item to sell.")
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def print_inventory():
+        #this prints out all avalible items in the stores inventory. 
+        if self.inventory:
+            for item in self.inventory: 
+                print( item, item.print_details())
+        else: 
+             print("No inventory to display.")
 
-    # What methods will you need?
+    # What methods will you need?- buy method, sell method, print inventory method

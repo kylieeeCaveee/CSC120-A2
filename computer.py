cc4e460594d63625a489dbbe525f177950c1e900
kylie <kcave@smith.edu>
2025-02-09 17:20:34 -0500

    day 4 work

--- computer.py
+++ computer.py
@@ -1,8 +1,9 @@
 from typing import Dict, Optional
 class Computer:
-    #This is the computer class- it has the attributes: desciption, processor_type, hard_drive_capactiy, 
+    #This is the computer class- it has the attributes: item_id, desciption, processor_type, hard_drive_capactiy, 
     # memory, operating_system, year_made, price. It contains the methods update_price, update_OS, 
-    # and refurbish, and print_details. 
+    # and refurbish, print_details, and get_name() function.
+    item_id: str
     description:str
     processor_type:str
     hard_drive_capacity: int
@@ -14,8 +15,9 @@ class Computer:
    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self, des:str, pt:str, hdc: int, mem: int, os: str, ym:int, p: int):
+    def __init__(self, itm_id: str, des:str, pt:str, hdc: int, mem: int, os: str, ym:int, p: int):
         #this initializes a new computer and all of it's corresponding variables
+        self.item_id=itm_id
         self.description= des
         self.processor_type= pt
         self.hard_drive_capacity=hdc
@@ -25,9 +27,12 @@ class Computer:
         self.price= p
         
     
-    def update_price(self, new_price: int): 
+    def update_price(self, itm_id: str, new_price: int): 
         #this updates the price of the computer to the price given to the function
-        self.price= new_price
+        if itm_id== self.item_id: 
+            self.price= new_price
+        else: 
+             print("Item", itm_id , "not found. Cannot update price.")
         
     def update_OS(self,new_OS: str): 
         #this updates the OS of the computer to the new OS given
@@ -49,16 +54,25 @@ class Computer:
     
     def print_details(self): 
         #this function prints all the attributes of a computer
-        print ("Description:", self.description, "Proccessor:", self.processor, "Hard Drive Capacity:", self.hard_drive_capacity, "Memory", self.memory, "Operating System:", self.operating_system, "Year:", self.year_made, "Price:", self.price)
+        print ("-----", self.item_id, "-------", "\n" "Description:", self.description, "\n" "Proccessor:", self.processor_type, "\n" "Hard Drive Capacity:", self.hard_drive_capacity, "\n" "Memory", self.memory, "\n" "Operating System:", self.operating_system, "\n" "Year:", self.year_made, "\n" "Price:", self.price, "\n" "---------------------")
+
+    def get_item_id(self):
+        #this method prints out the item_id of the computer
+        print (self.item_id, end= " ")
+        
 
 def main(): 
-    myComputer: Computer= Computer("my mac","some processor", 1024, 64 ,"old OS", 2013, 1500)
-    print (myComputer)
-    myComputer.update_price("1000000")
+    myComputer: Computer= Computer("my computer", "my mac","some processor", 1024, 64 ,"old OS", 2013, 1500)
+    myComputer.print_details()
+    myComputer.update_price("my computer", 10000)
     myComputer.update_OS("newest OS")
-    print(myComputer)
+    myComputer.print_details()
     myComputer.refurbish("epic OS")
-    print (myComputer)
+    myComputer.print_details()
+    otherComputer: Computer= Computer("someone elses computer", "other mac","some processor", 1024, 64 ,"old OS", 2013, 1500)
+    myComputer.update_price("otherComputer", 2000)
+    myComputer.get_name()
+    print("test")
 
 if __name__== "__main__": 
     main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,4 +1,4 @@
-import computer
+from computer import *
 
 class ResaleShop:
     #this is the class ResaleShop, it has the attribute inventory and the buy and sell and print inventory methods
@@ -8,25 +8,50 @@ class ResaleShop:
         #this initalizes any new resale shops
         self.inventory= ivn
     
-    def buy(self,comp: computer): 
+    def buy(self, itm_id: str, des:str, pt:str, hdc: int, mem: int, os: str, ym:int, p: int): 
+     
         #this buys the computer and adds it to the stores inventory
-        self.inventory.append(comp)
-        return self.inventory.index(comp)
+        itm_id: Computer = Computer (itm_id, des, pt, hdc, mem, os, ym, p)
+        self.inventory.append(itm_id)
+        
     
-    def sell(self,comp: computer):
+    def sell(self, itm_id: Computer):
         #this sells the computer and removes it from the stores inventory
-        if comp in self.inventory:  
-            self.inventory.pop(comp)
-            print("Item", comp, "sold!")
+        if itm_id in self.inventory:
+            current_computer_idx=self.inventory.index(itm_id)
+            self.inventory.pop(current_computer_idx)
+            
+            print("Item", end= " ")
+            itm_id.get_item_id()
+            print("sold!")
         else: 
-            print("Item", comp, "not found. Please select another item to sell.")
+            print("Item", end=" ")
+            itm_id.get_item_id()
+            print("not found. Please select another item to sell.")
 
     def print_inventory(self):
         #this prints out all avalible items in the stores inventory. 
         if self.inventory:
             for item in self.inventory: 
-                print( item, item.print_details())
+                item.print_details()
         else: 
              print("No inventory to display.")
 
-    # What methods will you need?- buy method, sell method, print inventory method
+def main(): 
+    computer_zero: Computer= Computer("computer_zero", "Mac Pro 2019", "some processor", 1050, 120, "Newest OS", 2019, 2000)
+    myresaleshop: ResaleShop= ResaleShop (ivn=[computer_zero])
+    myresaleshop.print_inventory()
+    myresaleshop.buy("computer_one", "Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    myresaleshop.print_inventory()
+    myresaleshop.sell(computer_zero)
+    myresaleshop.print_inventory()
+    other_resale_shop: ResaleShop= ResaleShop (ivn=[])
+    other_resale_shop.print_inventory()
+    other_resale_shop.sell(computer_zero)
+
+
+if __name__== "__main__": 
+    main()
+
+    
+
